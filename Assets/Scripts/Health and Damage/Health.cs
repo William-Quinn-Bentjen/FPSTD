d9e9ed620bd56bbfccea62d2ac6c49cb42b8797f
William Bentjen <s179051@students.aie.edu.au>
2018-04-17 17:52:36 -0700

    inital commit

---
+++ Assets/Scripts/Health and Damage/Health.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour, IDamageable {
+    public float minHP;
+    public float HP;
+    public float maxHP;
+
+    public delegate void OnDeath();
+    public OnDeath Death;
+    public void TakeDamage(float amount)
+    {
+        HP -= amount;
+        if (HP < maxHP)
+        {
+            Death.Invoke();
+        }
+    }
+    public void Heal(int amount)
+    {
+        HP += amount;
+        if (HP > maxHP)
+        {
+            HP = maxHP;
+        }
+    }
+    public void SetHP(float newHP)
+    {
+        if (newHP > maxHP)
+        {
+            HP = maxHP;
+        }
+        else if (newHP < minHP)
+        {
+            HP = minHP;
+        }
+        HP = newHP;
+    }
+}
---
+++ Assets/Scripts/Health and Damage/IDamageable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageable {
+    void TakeDamage(float amount);
+    void Heal(int amount);
+    void SetHP(float HP);
+}
